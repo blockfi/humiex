6dac7b6a560eb23cb2b6b80cc6dfe2e8407f7bdd
Eduardo Moraga <eduardo@blockfi.com>
2020-11-02 18:51:32 -0300

    add hex.pm config

--- mix.exs
+++ mix.exs
@@ -1,14 +1,19 @@
 defmodule Humiex.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :humiex,
-      version: "0.1.0",
+      description: description(),
+      version: @version,
       elixir: "~> 1.10",
-      elixirc_paths: elixirc_paths(Mix.env),
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -19,17 +24,44 @@ defmodule Humiex.MixProject do
     ]
   end
 
+  defp description() do
+    "Query or stream Humio (humio.com) events using it's rest search API"
+  end
+
+  defp package() do
+    [
+      name: "humiex",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* src),
+      licenses: ["MIT"],
+      maintainers: ["Eduardo Moraga"],
+      links: %{"GitHub" => "https://github.com/blockfi/humiex"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "Humiex",
+      source_ref: "v#{@version}",
+      canonical: "https://hexdocs.pm/humiex",
+      source_url: "https://github.com/blockfi/humiex",
+      extras: [
+        "README.md"
+      ]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
       {:httpoison, "1.2.0"},
       {:hackney, "1.16.0"},
-      {:jason, "1.2.2"}
+      {:jason, "1.2.2"},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib","test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
-
 end
